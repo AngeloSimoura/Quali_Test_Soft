612ed3961f570d2a4d30c839ddca925e1bf65cb5
Angelo <angelo.simoura@gmail.com>
2022-05-20 11:11:55 -0300

    form test done

--- teste_selenium.py
+++ teste_selenium.py
@@ -1,57 +1,78 @@
-#!/usr/bin/env python
-
+import time
 from selenium import webdriver
 
-from datetime import datetime
-from selenium.webdriver.chrome.service import Service
-from  webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
-# cria uma nova sessao no browser
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.chrome.service import Service
+
+from  webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+import unittest
+
+
 service = Service(executable_path=ChromeDriverManager().install())
 browser = webdriver.Chrome(service=service)
 
 browser.maximize_window()
+browser.implicitly_wait(10)
 
-# navega para a home page do Google
+browser.get("https://demoqa.com/")
+#Initial Page
+element=browser.find_element(By.XPATH,"//*[text()='Forms']")
+browser.execute_script("arguments[0].scrollIntoView();", element)
+element.click()
+#Forms Page
+practiceFormElement=browser.find_element(By.XPATH,"//*[text()='Practice Form']")
+practiceFormElement.click()
+#Practice Form Page
+firstNameElement=browser.find_element(By.ID,"firstName")
+lastNameElement=browser.find_element(By.ID,"lastName")
+txtMailElement=browser.find_element(By.ID,"userEmail")
+radioBTGenderFemale=browser.find_element(By.XPATH,"//*/label[text()='Female']")
+radioBTGenderMale=browser.find_element(By.XPATH,"//*/label[text()='Male']")
+radioBTGenderOther=browser.find_element(By.XPATH,"//*/label[text()='Other']")
+txtNumber=browser.find_element(By.ID,"userNumber")
+txtSubjects=browser.find_element(By.ID,"subjectsInput")
+checkBoxHobbiesSports=browser.find_element(By.XPATH,"//*/label[text()='Sports']")
+checkBoxHobbiesReading=browser.find_element(By.XPATH,"//*/label[text()='Reading']")
+checkBoxHobbiesMusic=browser.find_element(By.XPATH,"//*/label[text()='Music']")
+txtCurrentAddress=browser.find_element(By.ID,"currentAddress")
+selectState=browser.find_element(By.XPATH,"//*[text()='Select State']")
+selectCity=browser.find_element(By.XPATH,"//*[text()='Select City']")
+buttonSubmit=browser.find_element(By.ID,"submit")
+scrlDwnELement=browser.find_element(By.XPATH,"/html/body/div[2]/div/div/div[2]/div[1]/div/div/div[5]/span/div")
 
-browser.get("http://www.google.com/")
+firstNameElement.send_keys("Teste")
+lastNameElement.send_keys("Teste")
+txtMailElement.send_keys("teste@teste.com")
+radioBTGenderMale.click()
+txtNumber.send_keys("0123456789")
+txtSubjects.send_keys("Teste")
+browser.execute_script("arguments[0].scrollIntoView();", checkBoxHobbiesSports)
+checkBoxHobbiesSports.click()
+txtCurrentAddress.send_keys("Teste")
+browser.execute_script("arguments[0].scrollIntoView();", scrlDwnELement)
+scrlDwnELement.click()
+browser.execute_script("arguments[0].scrollIntoView();", selectState)
+selectState.click()
+valor='NCR'
+browser.find_element(By.XPATH,f'//*[text()="{valor}"]').click()
+browser.execute_script("arguments[0].scrollIntoView();", selectCity)
+selectCity.click()
+valor='Noida'
+browser.find_element(By.XPATH,f'//*[text()="{valor}"]').click()
+browser.execute_script("arguments[0].scrollIntoView();", buttonSubmit)
+buttonSubmit.click()
+wait=WebDriverWait(browser,10)
+titleSuccess=browser.find_element(By.ID,"example-modal-sizes-title-lg")
+wait.until(EC.visibility_of(titleSuccess))
+print(titleSuccess.text)
 
-# procura pelo elemento referente a searchbox
+time.sleep(30)
 
-search_field = browser.find_element_by_name("q")
 
-# envia a string @query para o element e realiza a busca
-
-query = "Test Driven Development"
-
-search_field.send_keys(query)
-
-search_field.submit()
-
-# espera até 3 segundos até o resultado ficar pronto
-
-browser.implicitly_wait(3) 
-
-# procura pelos resultados
-
-results = browser.find_elements_by_class_name("r")
-
-# para cada resultado (<div>) procura a ancora (<a>) que contem o link da pagina
-
-print("* {} primeiros resultados para a busca ‘{}’:".format(len(results), query))
-
-for index, r in enumerate(results):
-
-        link = r.find_elements_by_tag_name("a")[0].get_attribute("href")
-
-        print("\t{:2d}. {}".format(index + 1, link))
-
-# tira um print da pagina
-
-now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
-browser.save_screenshot('search_screenshot-{}.png'.format(now))
-
-# fecha a sessao com o browser
-
-browser.quit()
--- utest_1..py
+++ utest_1..py
@@ -1,3 +1,5 @@
+import os
+import time
 from selenium import webdriver
 
 from selenium.webdriver.common.keys import Keys
@@ -7,6 +9,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 from  webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.support.ui import Select
+
+from selenium.webdriver.common.keys import Keys
 
 import unittest
 
@@ -19,58 +29,113 @@ class PetPage(unittest.TestCase):
         
         inst.service = Service(executable_path=ChromeDriverManager().install())
         inst.browser = webdriver.Chrome(service=inst.service)
-
         inst.browser.implicitly_wait(10)
 
-        inst.browser.get("http://coral.ufsm.br/pet-si/")
+        inst.browser.maximize_window()
+        inst.wait=WebDriverWait(inst.browser,10)
+
+        inst.browser.get("https://demoqa.com/")
 
  
 
-    def test_01_open_publicacoes_page(self):
+    def test_01_open_forms_page(self):
 
-        self.browser.find_element_by_xpath("/html/body/main/div[2]/div/section/article/div/div/div/div/section[4]/div/div/div[2]/div/div/div/div/div/div[2]/h3/a").click()
-
-        self.assertIn('notfa/sfdsf', self.browser.current_url)
+        element=self.browser.find_element(By.XPATH,"//*[text()='Forms']")
+        self.browser.execute_script("arguments[0].scrollIntoView();", element)
+        element.click()
+        self.assertIn('forms', self.browser.current_url)
 
         
 
-    def test_02_search(self):
+    def test_02_click_practice_form_page(self):
 
-        # não é necessário navegar para 'Publicações' novamente
+        self.wait.until(EC.element_to_be_clickable(self.browser.find_element(By.XPATH,"//*[text()='Practice Form']")))
+        time.sleep(2)
+        practiceFormElement=self.browser.find_element(By.XPATH,"//*[text()='Practice Form']")
+        practiceFormElement.click()
 
-        self.search_field = self.browser.find_element_by_xpath("/html/body/header/div[1]/div[3]/div/div[2]/form/fieldset/div/input")
+        self.assertIn('automation-practice-form', self.browser.current_url)
 
-        self.search_field.send_keys("PET Redação")
-
-        self.search_field.send_keys(Keys.ENTER) # submit query
-
-        self.assertTrue(self.is_element_on_page(By.CLASS_NAME, 'only'))   # check for results
-
- 
+    def test_03_fill_form(self):
+        # get Elements
+        firstNameElement=self.browser.find_element(By.ID,"firstName")
+        lastNameElement=self.browser.find_element(By.ID,"lastName")
+        txtMailElement=self.browser.find_element(By.ID,"userEmail")
+        radioBTGenderFemale=self.browser.find_element(By.XPATH,"//*/label[text()='Female']")
+        radioBTGenderMale=self.browser.find_element(By.XPATH,"//*/label[text()='Male']")
+        radioBTGenderOther=self.browser.find_element(By.XPATH,"//*/label[text()='Other']")
+        txtNumber=self.browser.find_element(By.ID,"userNumber")
+        txtDateBirth=self.browser.find_element(By.ID,'dateOfBirthInput')
+        txtSubjects=self.browser.find_element(By.ID,"subjectsInput")
+        checkBoxHobbiesSports=self.browser.find_element(By.XPATH,"//*/label[text()='Sports']")
+        checkBoxHobbiesReading=self.browser.find_element(By.XPATH,"//*/label[text()='Reading']")
+        checkBoxHobbiesMusic=self.browser.find_element(By.XPATH,"//*/label[text()='Music']")
+        pictureElement=self.browser.find_element(By.ID,"uploadPicture")
+        txtCurrentAddress=self.browser.find_element(By.ID,"currentAddress")
+        selectState=self.browser.find_element(By.XPATH,"//*[text()='Select State']")
+        selectCity=self.browser.find_element(By.XPATH,"//*[text()='Select City']")
+        buttonSubmit=self.browser.find_element(By.ID,"submit")
+        scrlDwnELement=self.browser.find_element(By.XPATH,"/html/body/div[2]/div/div/div[2]/div[1]/div/div/div[5]/span/div")
+        
+        # fill Elements
+        firstNameElement.send_keys("Teste")
+        lastNameElement.send_keys("Teste")
+        txtMailElement.send_keys("teste@teste.com")
+        radioBTGenderMale.click()
+        txtNumber.send_keys("0123456789")
+        txtDateBirth.click()
+        self.wait.until(EC.element_to_be_clickable(self.browser.find_element(By.CLASS_NAME,"react-datepicker__month-select")))
+        monthPicker=Select(self.browser.find_element(By.CLASS_NAME,"react-datepicker__month-select"))
+        monthPicker.select_by_value('0')
+        self.wait.until(EC.element_to_be_clickable(self.browser.find_element(By.CLASS_NAME,"react-datepicker__year-select")))
+        yearPicker=Select(self.browser.find_element(By.CLASS_NAME,"react-datepicker__year-select"))
+        yearPicker.select_by_value('1998')
+        valor='10'
+        try:
+            dayPicker=self.browser.execute_script(f"return document.getElementsByClassName('react-datepicker__day react-datepicker__day--0{valor}')[0]")
+        except Exception as e:
+            dayPicker=self.browser.execute_script(f"return document.getElementsByClassName('react-datepicker__day react-datepicker__day--0{valor} react-datepicker__day--weekend')[0]")           
+        dayPicker.click()
+        txtSubjects.send_keys("Computer Science")
+        txtSubjects.send_keys(Keys.ENTER)
+        self.browser.execute_script("arguments[0].scrollIntoView();", scrlDwnELement)
+        self.browser.execute_script("arguments[0].scrollIntoView();", checkBoxHobbiesSports)
+        checkBoxHobbiesSports.click()
+        self.browser.execute_script("arguments[0].scrollIntoView();", pictureElement)
+        pictureElement.send_keys(os.getcwd()+"/picture.jpg")
+        time.sleep(1)
+        txtCurrentAddress.send_keys("Teste")
+        self.browser.execute_script("arguments[0].scrollIntoView();", scrlDwnELement)
+        scrlDwnELement.click()
+        self.browser.execute_script("arguments[0].scrollIntoView();", selectState)
+        try:
+            selectState.click()
+        except Exception as e:
+            self.browser.execute_script("arguments[0].scrollIntoView();", selectState)
+            selectState.click()
+        valor='NCR'
+        self.browser.find_element(By.XPATH,f'//*[text()="{valor}"]').click()
+        self.browser.execute_script("arguments[0].scrollIntoView();", selectCity)
+        time.sleep(1)
+        selectCity.click()
+        valor='Noida'
+        self.browser.find_element(By.XPATH,f'//*[text()="{valor}"]').click()
+        self.browser.execute_script("arguments[0].scrollIntoView();", buttonSubmit)
+        buttonSubmit.click()
+        
+    def test_04_check_title_success(self):
+        titleSuccess=self.browser.find_element(By.ID,"example-modal-sizes-title-lg")
+        self.wait.until(EC.visibility_of(titleSuccess))
+        print(titleSuccess.text)
 
     @classmethod
 
     def tearDownClass(inst):
 
        inst.browser.quit()
-
- 
-
-    def is_element_on_page(self, how, what):
-
-        try: 
-
-            self.browser.find_element(by=how, value=what)
-
-        except:
-
-            return False
-
-        return True
-
  
 if __name__ =='__main__':
         try:
-            unittest.main(verbosity=2)
+            unittest.main(verbosity=4)
         except SystemExit:
             pass
